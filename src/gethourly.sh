b56e91c0a48339aebf9515ff93be0fbe65f80561
cuongnvth <cuongnvth@users.noreply.github.com>
2020-12-01 10:18:57 +0700

    Update gethourly.sh

--- src/gethourly.sh
+++ src/gethourly.sh
@@ -10,10 +10,10 @@ while [ 1 ]
  do 
 	ping -c 1 -W 0.7 8.8.4.4 > /dev/null 2>&1
 	if [ $? -eq 0 ];then
-		wget --timeout=1 --waitretry=0 --tries=20 --retry-connrefused --ftp-user anonymous --ftp-password cuongnvth@gmail.com "ftps://gdc.cddis.eosdis.nasa.gov/gnss/data/hourly/$year""/$day""/hour""$day""0.$yr""n.Z";
-		# wget --timeout=1 --waitretry=0 --tries=20 --retry-connrefused "ftps://gdc.cddis.eosdis.nasa.gov/gnss/data/hourly/$year""/$day""/hour""$day""0.$yr""n.Z";
-		#wget --auth-no-challenge "https://cddis.gsfc.nasa.gov/gnss/data/hourly/$year""/$day""/hour""$day""0.$yr""n.Z";
-		# curl --connect-timeout 15 --retry 10 -u anonymous:cuongnvth@gmail.com -O --ftp-ssl "ftp://gdc.cddis.eosdis.nasa.gov/gnss/data/hourly/$year""/$day""/hour""$day""0.$yr""n.Z";
+# 		wget --timeout=1 --waitretry=0 --tries=20 --retry-connrefused --ftp-user anonymous --ftp-password cuongnvth@gmail.com "ftps://gdc.cddis.eosdis.nasa.gov/gnss/data/hourly/$year""/$day""/hour""$day""0.$yr""n.Z";
+# 		wget --timeout=1 --waitretry=0 --tries=20 --retry-connrefused "ftps://gdc.cddis.eosdis.nasa.gov/gnss/data/hourly/$year""/$day""/hour""$day""0.$yr""n.Z";
+# 		wget --auth-no-challenge "https://cddis.gsfc.nasa.gov/gnss/data/hourly/$year""/$day""/hour""$day""0.$yr""n.Z";
+		curl --connect-timeout 15 --retry 10 -u anonymous:cuongnvth@gmail.com -O --ftp-ssl "ftp://gdc.cddis.eosdis.nasa.gov/gnss/data/hourly/$year""/$day""/hour""$day""0.$yr""n.Z";
 		#wget --timeout=1 --waitretry=0 --tries=5 --retry-connrefused "ftp://cddis.gsfc.nasa.gov/gnss/data/hourly/$year""/$day""/hour""$day""0.$yr""n.Z";
 		if [ -f "hour""$day""0.$yr""n.Z" ]; then
 			echo "hour""$day""0.$yr""n.Z exists."
